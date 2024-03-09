4e18ab493d5b0b0a1592d83ff296b17a13d754ce
Moyses Azevedo <m.mirazev@gmail.com>
2024-03-09 19:52:09 -0300

    :feat: feature: add widget

---
+++ Revision/RevisionBundle.swift
@@ -0,0 +1,17 @@
+//
+//  RevisionBundle.swift
+//  Revision
+//
+//  Created by Moyses Miranda do Vale Azevedo on 09/03/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RevisionBundle: WidgetBundle {
+    var body: some Widget {
+        Revision()
+        RevisionLiveActivity()
+    }
+}
