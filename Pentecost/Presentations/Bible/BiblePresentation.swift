37d67de07b079465c3073944bd4ebf6df6758228
Moyses Azevedo <m.mirazev@gmail.com>
2024-03-13 10:54:46 -0300

    ♻️ refactor: modifier searchable

--- Pentecost/Presentations/Bible/BiblePresentation.swift
+++ Pentecost/Presentations/Bible/BiblePresentation.swift
@@ -6,22 +6,43 @@
 //
 
 import SwiftUI
+import SwiftUI
 
 struct BiblePresentation: View {
     let vm = BibleVM()
+    @State var searchBook: String = ""
 
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(0..<vm.bibleXML.books.count, id: \.self) { index in
-                    NavigationLink {
-                        BookPresentation(bibleXML: vm.bibleXML, number: index)
-                    } label: {
-                        Text(vm.bibleXML.nameBooks[index])
+            if searchBook != "" &&
+                vm.bibleXML.nameBooks.filter({ $0.contains(searchBook) }).count == 0 {
+                    noSerchBook
+                } else {
+                    List {
+                        ForEach(0..<vm.bibleXML.books.count, id: \.self) { index in
+                            if searchBook.isEmpty || vm.bibleXML.nameBooks[index].contains(searchBook) {
+                                navigationLink(for: index)
+                            }
+                        }
                     }
+                    .navigationTitle("Books")
                 }
-            }
-            .navigationTitle("Books")
+            
+
+        }
+        .searchable(text: $searchBook)
+    }
+
+    var noSerchBook: some View {
+        Text("No exist book with name in Bible")
+    }
+
+    @ViewBuilder
+    private func navigationLink(for index: Int) -> some View {
+        NavigationLink {
+            BookPresentation(bibleXML: vm.bibleXML, number: index)
+        } label: {
+            Text(vm.bibleXML.nameBooks[index])
         }
     }
 }
